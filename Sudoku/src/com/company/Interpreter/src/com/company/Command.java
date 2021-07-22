9390fa6b471805d7e839687f0a9238d7ae2bd8ea
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-22 22:43:46 +0300

    Delete Sudoku/src/com/company/Interpreter directory

--- Sudoku/src/com/company/Interpreter/src/com/company/Command.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public interface Command {
-    public String execute(String input);
-}
--- Sudoku/src/com/company/Interpreter/src/com/company/CountWordsCommand.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import java.util.*;
-public class CountWordsCommand implements Command{
-    public String execute(String input){
-        StringTokenizer st = new StringTokenizer(input, " ");
-        int count = st.countTokens();
-        return String.valueOf(count);
-    }
-}
