4a1c42d1c2b4f36879bcb8041a5ae75620746f03
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-26 00:56:20 +0300

    Update ReverseCommand.java

--- CommandInterpreter/src/main/java/ReverseCommand.java
+++ CommandInterpreter/src/main/java/ReverseCommand.java
@@ -1,6 +1,15 @@
 public class ReverseCommand implements Command{
     String name = "reverse";
     @Override
+    /**
+     * Returns the result of the execution of a reverse command.
+     *
+     * This method executes the reverse command and returns as a result
+     * a string with the input written backwards.
+     *
+     * @param  input  the string that is going to be reversed
+     * @return        the result of the execution as a string
+     */
     public String execute(String input){
         return new StringBuilder(input).reverse().toString();
     }
