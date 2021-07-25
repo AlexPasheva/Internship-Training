52560f84ee003ae271b1debd19455bb64f81ccaf
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-26 00:14:45 +0300

    Delete Interpreter/src/com/company directory

--- Interpreter/src/com/company/ReverseCommand.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class ReverseCommand implements Command{
-    @Override
-    public String execute(String input){
-        return new StringBuilder(input).reverse().toString();
-    }
-}
