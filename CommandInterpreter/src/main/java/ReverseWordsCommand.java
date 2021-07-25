312821410ce062d76dd8f1e97f31325f5d423be4
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-26 00:55:29 +0300

    Update ReverseWordsCommand.java

--- CommandInterpreter/src/main/java/ReverseWordsCommand.java
+++ CommandInterpreter/src/main/java/ReverseWordsCommand.java
@@ -2,6 +2,15 @@ import java.util.*;
 public class ReverseWordsCommand implements Command{
     String name = "reverse-words";
     @Override
+    /**
+     * Returns the result of the execution of a reverse-words command.
+     *
+     * This method executes the reverse-words command and returns as a result
+     * a string with the words of the input but in reversed order.
+     *
+     * @param  input  the string that is going to be reversed by words
+     * @return        the result of the execution as a string
+     */
     public String execute(String input){
         StringTokenizer st = new StringTokenizer(input, " ");
         String reversedLine = "";
