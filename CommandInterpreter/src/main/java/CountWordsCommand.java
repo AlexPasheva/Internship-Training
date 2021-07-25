729b93a164d7fc2ae1ae60269f56977e0de545fd
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-26 00:55:59 +0300

    Update CountWordsCommand.java

--- CommandInterpreter/src/main/java/CountWordsCommand.java
+++ CommandInterpreter/src/main/java/CountWordsCommand.java
@@ -2,6 +2,15 @@ import java.util.*;
 public class CountWordsCommand implements Command{
     String name = "count-words";
     @Override
+    /**
+     * Returns the result of the execution of a count-words command.
+     *
+     * This method executes the count-words command and returns as a result
+     * a string with the number of words the input has.
+     *
+     * @param  input  the string that is going to be word-counted
+     * @return        the result of the execution as a string
+     */
     public String execute(String input){
         StringTokenizer st = new StringTokenizer(input, " ");
         int count = st.countTokens();
