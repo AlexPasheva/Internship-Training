d408b1b4bbaabe2b2d98aed97f27f17af1a7e1cf
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-07-26 17:41:57 +0300

    Update CommandEngine.java

--- CommandInterpreter/src/main/java/CommandEngine.java
+++ CommandInterpreter/src/main/java/CommandEngine.java
@@ -1,43 +1,43 @@
 import java.util.*;
 
+/**
+ * An engine that executes the commands
+ */
 public class CommandEngine {
-    private final Map<String, Command> Commands = new HashMap<String, Command>();
+    private final Map<String, Command> commands = new HashMap<String, Command>();
     /**
      * This method fills the map with available commands and is completely wrong
      */
     private void fillMap() {
-        this.Commands.put("reverse", new ReverseCommand());
-        this.Commands.put("reverse-words", new ReverseCommand());
-        this.Commands.put("count-words", new ReverseCommand());
+        List<Command> commandList = Arrays.asList(new ReverseCommand(),
+                                                  new ReverseWordsCommand(),
+                                                  new CountWordsCommand());
+        for (Command cmd: commandList) {
+            commands.put(cmd.getName(), cmd);
+        }
     }
     /**
-     * Returns a Command object if it exists in the map
-     *
-     * This method can throw an Illegal Argument Exception if the name of the command
-     * does not exist in the map of available commands. If the command exists then an
-     * instance of a command with the same name is returned.
+     * This method can finds and returns the right command obj by passing it's name
      *
      * @param  commandName  the name of the command
      * @return              the right command object
+     * @throws              IllegalArgumentException if the name of the command
+     *                      does not exist in the map of available commands.
      */
     private Command findCommand(String commandName){
-        if(Commands.containsKey(commandName)) {
-            return Commands.get(commandName);
+        if(commands.containsKey(commandName)) {
+            return commands.get(commandName);
         } else {
             throw new IllegalArgumentException("No command " + commandName);
         }
     }
     /**
-     * Returns the result of the execution of a given command
-     *
-     * This method can throw an Illegal Argument Exception that comes from trying to
-     * find the command name in the map of available commands. If the command is
-     * found on the map then it's going to executed and the result of the execution
-     * is going to be returned as a string
+     * This method executes the command
      *
      * @param  commandName the name of the command that is going to be executed
      * @param  arguments   the arguments that the command is going to be executed on
      * @return             the result of the execution as a string
+     * @throws              IllegalArgumentException if a command with that name does not exists
      */
     public String execute(String commandName, String arguments) {
         fillMap();
