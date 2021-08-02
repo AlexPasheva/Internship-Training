f56387fd0367a8612a056efc5ee19ff5ee797c2d
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-08-03 00:24:13 +0300

    Delete Subtraction.java

--- CommandInterpreter/src/main/java/Subtraction.java
+++
@@ -1,9 +0,0 @@
-public class Subtraction implements Operation {
-    private static final String NAME = "-";
-    public String getName() {
-        return NAME;
-    }
-    public Variable execute(String operation, Variable operand1, Variable operand2) {
-
-    }
-}
