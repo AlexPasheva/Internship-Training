1ac141ee0af48b6bc692fea0d7049cf79649a945
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-08-11 00:27:55 +0300

    Delete ConverterTask/src/main/java/Converter/src/main/java/Converter directory

--- ConverterTask/src/main/java/Converter/src/main/java/Converter/Converter.java
+++
@@ -1,8 +0,0 @@
-package Converter;
-import java.util.Collection;
-
-public interface Converter {
-    Class<?> getClassFrom();
-    Class<?> getClassTo();
-    Object convert(Object obj, Class<?> targetClass);
-}
