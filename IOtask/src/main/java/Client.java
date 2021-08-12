55e9800f651c2b36ea6d09bb398761d6e7f16779
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2021-08-12 10:56:01 +0300

    Update Client.java

--- IOtask/src/main/java/Client.java
+++ IOtask/src/main/java/Client.java
@@ -7,20 +7,11 @@ public class Client {
     public void start(int port) {
         try (Socket socket = new Socket("localhost", port);
              OutputStream outputStream = socket.getOutputStream();
-             PrintWriter printWriter = new PrintWriter(outputStream, true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
+             PrintWriter printWriter = new PrintWriter(outputStream, true);) {
 
             out.println("(client) Connecting..");
             printWriter.write(340);
             
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                if (".".equals(inputLine)) {
-                    out.println("good bye");
-                    break;
-                }
-                out.println(inputLine);
-            }
 
         } catch (IOException e) {
             e.printStackTrace();
